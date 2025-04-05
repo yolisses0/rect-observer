3a981dca1c3a76eec4fd486b9cc02d39d7ff4ea7
Yolisses <yolisses0@gmail.com>
2025-04-05 14:02:40 -0300

    Delete add.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import { add } from "./add";
 import { RectObserver } from "./RectObserver";
 import type { RectObserverCallback } from "./RectObserverCallback";
 import { RectObserverEntry } from "./RectObserverEntry";
 import type { RectObserverInit } from "./RectObserverInit";
 
 export {
-  add,
   RectObserver,
   RectObserverCallback,
   RectObserverEntry,
