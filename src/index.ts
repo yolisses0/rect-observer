e45c827a5492085a06bb68b073c455de9b8a6870
Yolisses <yolisses0@gmail.com>
2025-04-06 06:48:14 -0300

    Remove RectObserverEntry from index.ts export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import { RectObserver } from "./RectObserver";
 import type { RectObserverCallback } from "./RectObserverCallback";
-import { RectObserverEntry } from "./RectObserverEntry";
 import type { RectObserverInit } from "./RectObserverInit";
 
-export {
-  RectObserver,
-  RectObserverCallback,
-  RectObserverEntry,
-  RectObserverInit,
-};
+export { RectObserver, RectObserverCallback, RectObserverInit };
